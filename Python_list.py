1ebb4e6f4d088e0cd8c17209f642faee81760986
zhaoxukun <150403725+John-Zxk@users.noreply.github.com>
2026-02-02 14:58:30 +0800

    Enhance linked list operations with insertions#1
    Added functionality to insert elements at the end and middle of the linked list.

--- Python_list.py
+++ Python_list.py
@@ -30,8 +30,48 @@ head = createLinkedList([1, 2, 3, 4, 5])
 newNode = ListNode(0)
 newNode.next = head
 head = newNode
+
 # 遍历单链表
 p = head
 while p is not None:
     print(p.val)
     p = p.next
+    
+# 在单链表尾部插入新元素
+
+head = createLinkedList([1, 2, 3, 4, 5])
+p = head 
+while p.next is not None:
+    p = p.next
+
+p.next = ListNode(6)
+
+
+p = head 
+while p is not None:
+    print(p.val)
+    p = p.next
+
+
+# 在单链表中间插入新元素
+head = createLinkedList([1, 2, 3, 4, 5])
+p = head
+for i in range(0, 2):
+    print(p.val)
+    p = p.next
+   
+# 此时 p 指向第 3 个节点
+# 组装新节点的后驱指针
+new_node = ListNode(66)
+new_node.next = p.next
+
+# 插入新节点
+p.next = new_node
+
+# 现在链表变成了 1 -> 2 -> 3 -> 66 -> 4 -> 5
+
+p = head 
+while p is not None:
+    print(p.val)
+    p = p.next
+
